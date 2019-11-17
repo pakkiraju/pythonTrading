0043d1b1fed23a22263a845e1a16bb87e3b147dc
pakkiraju <prad.akkiraju@gmail.com>
2019-11-17 02:12:09 -0500

    Fixed Comments

--- yahooFinanceVersion1_pandas.py
+++ yahooFinanceVersion1_pandas.py
@@ -1,23 +1,23 @@
-# import csv
-# import pandas as pd
-# from datetime import date, datetime
-#
-# earnings = pd.read_html('https://finance.yahoo.com/calendar/earnings?day=2019-11-19')[0]
-#
-# # if datetime.now().strftime("%H%M%S") >= "090000":
-# #     fileName = 'AMC_earnings_{}'.format(date.today())
-# # elif datetime.now().strftime("%H%M%S") < "090000":
-# #     fileName = 'BMO_earnings_{}'.format(date.today())
-#
-# fileName = "yahooFinance_Pandas"
-# with open(fileName + ".csv", mode='w') as csv_file:
-#     writer = csv.writer(csv_file)
-#     writer.writerow([earnings])
-#
-# print(earnings)
-
+import csv
 import pandas as pd
-url = r'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-tables = pd.read_html(url) # Returns list of all tables on page
-sp500_table = tables[0] # Select table of interest
-print(sp500_table)
+from datetime import date, datetime
+
+earnings = pd.read_html('https://finance.yahoo.com/calendar/earnings')[0]
+
+# if datetime.now().strftime("%H%M%S") >= "090000":
+#     fileName = 'AMC_earnings_{}'.format(date.today())
+# elif datetime.now().strftime("%H%M%S") < "090000":
+#     fileName = 'BMO_earnings_{}'.format(date.today())
+
+fileName = "yahooFinance_Pandas"
+with open(fileName + ".csv", mode='w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow([earnings])
+
+print(earnings)
+
+# import pandas as pd
+# url = r'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+# tables = pd.read_html(url) # Returns list of all tables on page
+# sp500_table = tables[0] # Select table of interest
+# print(sp500_table)
