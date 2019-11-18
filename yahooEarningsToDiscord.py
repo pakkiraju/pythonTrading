917ebde39e494e49a3510287dbde8f2f2dd94aba
Prad <prad.akkiraju@gmail.com>
2019-11-18 13:21:35 -0500

    pushing changes

--- yahooEarningsToDiscord.py
+++ yahooEarningsToDiscord.py
@@ -1,10 +1,12 @@
 # Work with Python 3.6
+from datetime import date
+
 import discord
 import pandas as pd
 from discord import channel
 from discord import client
 
-TOKEN = 'NjQ1OTAxNjE0MTQ5Nzk1ODQw.XdJsVQ.CVXfWwlCCVZtWoidjKv_5rKDtJs'
+TOKEN = 'xxx'
 
 client = discord.Client()
 
@@ -14,9 +16,11 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == "##er":
-        pd.option_context('display.max_rows', True, 'display.max_columns', True)
+        pd.option_context('display.max_rows', None, 'display.max_columns', None)
         earnings = pd.read_html('https://finance.yahoo.com/calendar/earnings')[0]
-        await message.channel.send(earnings)
+        earnings.to_csv('testfile.csv') #r'earnings_{}.csv'.format(date.today()), index=None)
+        await message.channel.send_file('testfile.csv')
+        # await message.channel.send(earnings)
 
 
 @client.event
@@ -26,7 +30,7 @@ async def on_ready():
     print(client.user.id)
     print('------')
     print('The bot is ready!')
-    game = discord.Game("with the API")
+    game = discord.Game("##er")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
